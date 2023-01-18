349353b762c478a991e3504e7fc074bdd7c57802
Mrityunjay3 <44922081+Mrityunjay3@users.noreply.github.com>
2023-01-18 23:20:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import React from "react";
+
+/** 
+  // Sample Response
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org"
+  }
+**/
+function App() {}
+
+export default App;
